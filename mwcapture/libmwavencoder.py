96fbf750dd0144c4fce0c18a151162885f1b0d01
sunhanxue <2570496023@qq.com>
2021-09-08 16:26:10 +0800

    HDMI音视频采集，首次提交

---
+++ mwcapture/libmwavencoder.py
@@ -0,0 +1,156 @@
+import platform
+import os
+import sys
+from ctypes import *
+
+MW_V_FOURCC_FMT_NV12 = 25
+MW_A_SAMPLE_FMT_S16 = 1
+MW_V_CODEC_H264 = 28
+MW_V_CODEC_H265 = 174
+
+MW_VENC_FRAME_TYPE_UNKNOWN = 0
+MW_VENC_FRAME_TYPE_IDR = 1
+MW_VENC_FRAME_TYPE_I = 2
+MW_VENC_FRAME_TYPE_P = 3
+MW_VENC_FRAME_TYPE_B = 4
+MW_VENC_FRAME_TYPE_COUNT = 5
+
+
+class mw_venc_frame_info(Structure):
+    _pack_ = 1
+    _fields_ = [('frame_type', c_int32),
+                ('delay', c_int32),
+                ('pts', c_int64)]
+
+mw_encode_callback = CFUNCTYPE(None,py_object,c_void_p,c_int32,c_int32,c_int32,c_int64)
+
+class mw_av_encoder(object):
+    def __init__(self):
+        super().__init__()
+        self.m_lib_av_encoder = 0
+        t_bits, t_linkage = platform.architecture()
+        self.m_abs_path = os.getcwd()
+        if platform.system()=='Windows':
+            if t_bits == '64bit':
+                # self.m_lib_path = "mwcapture\\bin\\x64\\mw_av_encoderd.dll"
+                self.m_lib_path = "mwcapture\\bin\\x64\\"
+                self.m_lib_path = os.getcwd() + "\\" + self.m_lib_path
+            elif t_bits == '32bit':
+                # self.m_lib_path = "mwcapture\\bin\\x86\\mw_av_encoderd.dll"
+                self.m_lib_path = "mwcapture\\bin\\x86\\"
+                self.m_lib_path = os.getcwd() + "\\" + self.m_lib_path
+            else:
+                raise AssertionError("ERROR:Current Source doesn't support %s" % (t_bits))
+            try:
+                os.chdir(self.m_lib_path)
+                self.m_lib_av_encoder = cdll.LoadLibrary("mw_av_encoderd.dll")
+                os.chdir(self.m_abs_path)
+            except OSError as identifier:
+                os.chdir(self.m_abs_path)
+                raise AssertionError("ERROR:load %s failed - %s\n" % (self.m_lib_path, identifier))
+            else:
+                self.load_win_funcs()
+        else:
+            raise AssertionError("ERROR:Current Source doesn't support %s" % (t_bits))
+
+    def load_win_funcs(self):
+        self.m_lib_av_encoder.mw_video_encoder_open.restype = c_void_p
+        self.m_lib_av_encoder.mw_video_encoder_open.argtypes = [
+            c_int32,
+            c_int32,
+            c_uint32,
+            c_int32,
+            c_int32,
+            c_int32,
+            c_int32,
+            mw_encode_callback,
+            py_object]
+        self.m_lib_av_encoder.mw_video_encode_frame.argtypes = [
+            c_void_p,
+            c_void_p,
+            c_int64]
+        self.m_lib_av_encoder.mw_video_encoder_close.argtypes = [c_void_p]
+        self.m_lib_av_encoder.mw_audio_encoder_open.restype = c_void_p
+        self.m_lib_av_encoder.mw_audio_encoder_open.argtypes = [
+            c_uint32,
+            c_uint32,
+            c_uint32,
+            c_uint32,
+            mw_encode_callback,
+            py_object]
+        self.m_lib_av_encoder.mw_audio_encode_frame.argtypes = [
+            c_void_p,
+            c_voidp,
+            c_uint32,
+            c_int64]
+        self.m_lib_av_encoder.mw_audio_encoder_close.argtypes = [c_void_p]
+
+    def mw_avenc_init(self):
+        if self.m_lib_av_encoder == 0:
+            return -1
+        self.m_lib_av_encoder.mw_av_encode_init()
+        return 0
+
+    def mw_venc_encoder_open(self,
+        width,
+        height,
+        fourcc,
+        bitrate,
+        fps,
+        gopsize,
+        ish265,
+        callback,
+        param):
+        if self.m_lib_av_encoder == 0:
+            return -1
+        return self.m_lib_av_encoder.mw_video_encoder_open(
+            width,
+            height,
+            fourcc,
+            bitrate,
+            fps,
+            gopsize,
+            ish265,
+            callback,
+            param
+        )
+
+
+    def mw_venc_encode_frame(self,hvenc,data,ts):
+        if self.m_lib_av_encoder == 0:
+            return -1
+        return self.m_lib_av_encoder.mw_video_encode_frame(
+            hvenc,data,ts)
+
+    def mw_venc_encoder_close(self,hvenc):
+        if self.m_lib_av_encoder == 0:
+            return -1
+        return self.m_lib_av_encoder.mw_video_encoder_close(hvenc)
+
+    def mw_aenc_encoder_open(self,
+        channels,
+        smaplerate,
+        bitspersample,
+        bitrate,
+        callback,
+        param):
+        if self.m_lib_av_encoder == 0:
+            return -1
+        return self.m_lib_av_encoder.mw_audio_encoder_open(
+            channels,
+            smaplerate,
+            bitspersample,
+            bitrate,
+            callback,
+            param
+        )
+
+    def mw_aenc_encode_frame(self,aenc,data,datalen,ts):
+        if self.m_lib_av_encoder == 0:
+            return -1
+        return self.m_lib_av_encoder.mw_audio_encode_frame(aenc,data,datalen,ts)
+
+    def mw_aenc_encoder_close(self,aenc):
+        if self.m_lib_av_encoder == 0:
+            return -1
+        return self.m_lib_av_encoder.mw_audio_encoder_close(aenc)
